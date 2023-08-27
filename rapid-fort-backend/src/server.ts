d450bfab929dfe45f60efb18000664daf497e0bd
Ujjawal <ujjawalrachhoya100@gmail.com>
2023-08-28 00:54:16 +0530

    specifi node version

--- rapid-fort-backend/src/server.ts
+++ rapid-fort-backend/src/server.ts
@@ -25,7 +25,7 @@ app.use("/upload", uploadRouter);
 app.use("/files", fileRouter);
 app.use("/city", cityRouter);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
 });
