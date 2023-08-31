ec3b80177b568ca04afa2f70832c1d025878cdc6
Ujjawal <ujjawalrachhoya100@gmail.com>
2023-08-31 21:40:55 +0530

    only api route change

--- rapid-fort-frontend/apiRoute.ts
+++ rapid-fort-frontend/apiRoute.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://rapid-ford-api.onrender.com";
-// const baseUrl = "http://35.202.124.42";
+// const baseUrl = "https://rapid-ford-api.onrender.com";
+const baseUrl = "http://35.202.124.42";
 
 export default baseUrl;
